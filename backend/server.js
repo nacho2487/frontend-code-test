bde48e385c919200a8aee3bdaf9cfd349cf9364f
Nicolás Cardelino <ncardeli@gmail.com>
2019-10-01 17:33:25 -0300

    Add http request logger

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const router = require("./api/router");
+const httpLogger = require("morgan");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const httpLoggerFormat = isProduction ? "combined" : "dev";
+app.use(httpLogger(httpLoggerFormat));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +19,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use("/api", router);
 
-if (process.env.NODE_ENV === "production") {
+if (isProduction) {
 	// Serve any static files
 	app.use(express.static(path.join(__dirname, "../frontend/build")));
 
